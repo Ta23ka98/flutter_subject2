3643c2c097465e78797ad8f2680c05c5ae527eea
まさたか <shinonome419@icloud.com>
2023-03-03 15:45:56 +0900

    fix: 色指定を削除し、ダークテーマを適用

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,9 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        backgroundColor: Colors.grey[800],
         title: const Text(
           "ホーム",
           style: TextStyle(fontWeight: FontWeight.bold),
--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+      theme: ThemeData.dark(),
+      home: HomePage(),
     );
   }
 }
