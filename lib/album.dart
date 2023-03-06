4d3cd34c403ea5a4b537faff4683dcbdf808c276
まさたか <shinonome419@icloud.com>
2023-03-06 21:38:06 +0900

    add: Albumクラスを追加

---
+++ lib/album.dart
@@ -0,0 +1,9 @@
+class Album {
+  Album(
+      {required String imagePath,
+      required String title,
+      required String artist});
+  String? imagePath;
+  String? title;
+  String? artist;
+}
