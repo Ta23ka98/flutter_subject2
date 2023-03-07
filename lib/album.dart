33305f2508f1241c144b936cf2504e21ff28cc9c
まさたか <shinonome419@icloud.com>
2023-03-07 13:39:14 +0900

    fix: Albumモデルを修正

--- lib/album.dart
+++ lib/album.dart
@@ -1,9 +1,10 @@
 class Album {
-  Album(
-      {required String imagePath,
-      required String title,
-      required String artist});
-  String? imagePath;
-  String? title;
-  String? artist;
+  Album({
+    required this.imagePath,
+    required this.title,
+    required this.artist,
+  });
+  final String imagePath;
+  final String title;
+  final String artist;
 }
